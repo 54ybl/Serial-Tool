5a6fb6a9648be59bedd6f6dde54001206b7a3eef
Neptune <1353890723@qq.com>
2023-12-22 16:53:17 +0800

    add choose selection by number

--- menu.c
+++ menu.c
@@ -3,67 +3,62 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#define max_index 3
-
 void set_noncanonical_mode();
 void restore_terminal();
 void menu_show();
-void highlight();
 void exe();
+void listen();
 
-/*void menu(int index)
-  {
-
-  }
-
-  int highlight()
-  {
-
-  }*/
-
+/* Menu struct */
 struct MenuItem{
     int optionNum;
     char text[50];
     char cmd[50];
 };
 
+/* Menu content & command */
 struct MenuItem menu[] = {
     {0, "Ifconfig", "ifconfig syn0 192.168.1.25"},
     {1, "Fload", "fload tftp://192.168.1.35/gzrom.bin"},
     {2, "Reboot", "reboot"},
+    {3, "Poweroff", "poweroff"},
 };
 
 int main()
 {
-    set_noncanonical_mode();
+    set_noncanonical_mode();	//Set non-canonical mode
 
     system("clear");
 
     int index = 0;
     char input;
-    int itemCnt = sizeof(menu) / sizeof(menu[0]);
+    int itemCnt = sizeof(menu) / sizeof(menu[0]);	//Num of menu items
 
     menu_show(index, itemCnt);
+    printf("index: %d\n", index);
     while(1){
+	/* Read Enter */
 	read(STDIN_FILENO, &input, 1);
-	if(input == '\n')
+	if(input == '\n'){
 	    exe(index);
+	}
 
-	read(STDIN_FILENO, &input, 1);
-	if(input == 'q'){
+	/* Read q */
+	else if(input == 'q'){
 	    system("clear");
 	    break;
 	}
+
+	/* Read Up/Down */
 	else if(input == 27){
 	    read(STDIN_FILENO, &input, 1);
-
 	    if(input == 91){
 		read(STDIN_FILENO, &input, 1);
 
 		switch(input){
 		    case 'B':
 			//printf("Up\n");
-			if(index >= 2){
+			if(index >= (itemCnt - 1)){
 			    index = 0;
 			    break;
 			}
@@ -72,7 +67,7 @@ int main()
 		    case 'A':
 			//printf("Down\n");
 			if(index <= 0){
-			    index = 2;
+			    index = (itemCnt - 1);
 			    break;
 			}
 			index--;
@@ -86,19 +81,20 @@ int main()
 		}
 		//system("clear");
 		menu_show(index, itemCnt);
-		//highlight(index);
-		printf("index: %d\n", index);
+		//printf("index: %d\n", index);
 	    }
+	}
+	else if(input >= '0' && input <= '9'){
+	    printf("num: %d\n",input - '0');
+	    index = input - '0';
+	    menu_show(index, itemCnt);
 	}else{
 	    printf("%c pressed\n", input);
 	}
-
+	printf("index: %d\n", index);
     }
     restore_terminal();
 
-    printf("\033[31m1. ifconfig\033[0m\n");
-    printf("2. fload\n");
-    printf("3. reboot\n");
     return 0;
 }
 
@@ -118,21 +114,19 @@ void restore_terminal()
     tcsetattr(STDIN_FILENO, TCSANOW, &t);
 }
 
-void highlight(int index)
-{
-    int i = index;
-    char *text = menu[i].text;
-
-    printf("text: %s\n",text);
-}
-
 void exe(int index)
 {
     char cmd[128];
-    struct MenuItem *itemSel = &menu[index];
-    snprintf(cmd, sizeof(cmd), "echo \"%s\" > /dev/ttyUSB0",itemSel -> cmd);
+    //struct MenuItem *itemSel = &menu[index];
+    //snprintf(cmd, sizeof(cmd), "echo \"%s\" > /dev/ttyUSB0",itemSel -> cmd);
+    snprintf(cmd, sizeof(cmd), "echo \"%s\" > /dev/ttyUSB0",menu[index].text);
     printf("cmd :%s\n",cmd);
     system(cmd);
+}
+
+void listen()
+{
+    while()
 }
 
 void menu_show(int index,int itemCnt)
@@ -140,9 +134,9 @@ void menu_show(int index,int itemCnt)
     system("clear");
     for (int i = 0; i < itemCnt; ++i){
 	if(i == index){
-	    printf("\033[31m%d,%s\033[0m\n",menu[i].optionNum,menu[i].text);
+	    printf("\033[31m%d.%s <\033[0m\n",menu[i].optionNum,menu[i].text);
 	}else{
-	    printf("%d,%s\n",menu[i].optionNum,menu[i].text);
+	    printf("%d.%s\n",menu[i].optionNum,menu[i].text);
 	}
     }
 }
